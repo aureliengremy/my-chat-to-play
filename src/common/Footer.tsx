aac74514fc516b23facd138442113298f550da80
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-03-16 19:37:24 -0400

    feat(Footer) create and style the footer + add icon

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {UserRick, UserButter, IconButter} from '@/common/ImgVariable'
 
 type Props = {}
 
@@ -9,7 +10,7 @@ const Footer = (props: Props) => {
     <div className="w-full container mx-auto p-4 md:px-6 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-                <img src="" className="h-8 mr-3" alt="Flowbite Logo" />
+                <img src={IconButter} className="h-8 mr-3" alt="Flowbite Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ButterChat</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
