87281bc09a4c068504e8442b7acdae9d5875be5e
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-03-16 18:35:23 -0400

    init project - NextJS w/TailwindCSS full install

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPENAI_TOKEN: process.env.OPENAI_TOKEN,
+   },
 }
 
 module.exports = nextConfig
