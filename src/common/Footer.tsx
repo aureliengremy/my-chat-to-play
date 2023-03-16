e8383bde5f07805f2880d1c1eb181550bf3e6a8b
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-03-16 19:37:12 -0400

    feat(Footer) create and style the footer

---
+++ src/common/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    
+<footer className="bg-white shadow dark:bg-gray-900">
+    <div className="w-full container mx-auto p-4 md:px-6 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+                <img src="" className="h-8 mr-3" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ButterChat</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 3 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
+  )
+}
+
+export default Footer
