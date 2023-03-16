ac2998977a9281b96fc434c22ddb77cc421aa0c6
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-03-16 19:38:57 -0400

    feat(ImgVariable) some variable to use in component like image or info

---
+++ src/common/ImgVariable.tsx
@@ -0,0 +1,8 @@
+const UserRick: String =
+  "https://icon-library.com/images/rick-sanchez-icon/rick-sanchez-icon-25.jpg";
+const UserButter: String =
+  "https://fab-user.s3.ap-northeast-2.amazonaws.com/avatar/e8aef356ca5106110da60e409d1b84e4/6C2851F9DD2E54F159E4374AAA8C6FE8.png";
+const IconButter: String =
+  "https://passmethebutter.dev/img/butterbot_squarer.png";
+
+export { UserRick, UserButter, IconButter };
