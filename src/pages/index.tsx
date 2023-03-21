a36d30a76f2484a4a307154b1409a012b66b0001
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-03-21 18:08:13 -0400

    feat(index) comment the InputChat.tsx component for the moment - WILL CHANGE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <div className="w-full
             p-6 bg-white border border-gray-200 rounded-lg shadow">
               <Chat />
-              <InputChat />
+              {/* <InputChat /> */}
             </div>
 
           </div>
